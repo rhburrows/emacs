eb0e1293887e1cdafcf2c89c34c7e94cbccf7da0
Ryan Burrows <rhburrows@gmail.com>
2008-08-10 10:13:43 -0700

    added comment about Growl

--- rhb_mac_config.el
+++ rhb_mac_config.el
@@ -5,6 +5,8 @@
 
 (global-set-key "\C-cm" 'mac-toggle-max-window)
 
-;; Support for growl
+;;
+;; Support for growl for notifications
+;; http://growl.info/
 (add-to-list 'load-path "~/emacs/growl")
 (require 'rhb_growl_config)
