224c966fbfcb871a6644704e1c56ce6f2aa97cde
Ryan Burrows <rhburrows@gmail.com>
2008-11-04 12:58:12 -0800

    A few more tweaks

--- lisp/rhb_lisp_config.el
+++ lisp/rhb_lisp_config.el
@@ -1,7 +1,7 @@
 (provide 'rhb_lisp_config)
 
 ;; Define the lisp to use
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-lisp-implementations '((sbcl ("/usr/local/bin/sbcl"))))
 
 (add-to-list 'load-path "~/emacs/lisp/slime")
 (require 'slime)
