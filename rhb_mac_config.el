dc4172d2d16bd8772fc45002a8590ae4a19b6e9c
Ryan Burrows <rhburrows@gmail.com>
2008-11-04 12:58:11 -0800

    A few more tweaks

--- rhb_mac_config.el
+++ rhb_mac_config.el
@@ -10,3 +10,31 @@
 ;; http://growl.info/
 (add-to-list 'load-path "~/emacs/growl")
 (require 'rhb_growl_config)
+
+;;
+;; Commands to run things in Mac's terminal
+;; http://gist.github.com/11310
+(defun tell-terminal (commands)
+  "Use osascript to tell Terminal.app to do stuff"
+  (interactive)
+  (shell-command
+   (concat
+    "osascript"
+    (mapconcat
+     '(lambda (command) (concat " -e 'tell application \"Terminal\" "
+				command "'"))
+     commands ""))))
+
+(defun terminal-there (dir)
+  "Open Terminal.app and change working directory."
+  (interactive)
+  (tell-terminal (list
+		  (concat "to do script \"cd " dir "\"")
+		  "to activate")))
+
+(defun terminal-here ()
+  "Open Terminal.app in `default-directory'."
+  (interactive)
+  (terminal-there default-directory))
+
+; (global-set-key "\C-c\C-t" 'terminal-here)
