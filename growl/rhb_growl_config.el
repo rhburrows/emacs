b535fc84e04cc62c49d5c7681a7009e358b79001
Ryan Burrows <rhburrows@gmail.com>
2008-11-04 12:58:12 -0800

    A few more tweaks

--- growl/rhb_growl_config.el
+++ growl/rhb_growl_config.el
@@ -1,3 +1,13 @@
 (provide 'rhb_growl_config)
 
 (require 'growl)
+
+;; Hook in erc to use growl for notifications
+(defun my-erc-hook (match-type nick message)
+  "Shows a growl notification, when users' nick is mentioned"
+  (and (eq match-type 'current-nick)
+       (growl (concat "Your nick was mentioned on: "
+		      (buffer-name (current-buffer)))
+	      message)))
+
+(add-hook 'erc-text-matched-hook 'my-erc-hook)
