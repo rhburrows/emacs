12d17d1c766b93b55bbe78d946ec89d151190ba5
Ryan Burrows <rhburrows@gmail.com>
2008-12-28 11:49:05 -0800

    Added shortcut method for posting to my blog

--- g-client/rhb_g_config.el
+++ g-client/rhb_g_config.el
@@ -1,3 +1,9 @@
 (provide 'rhb_g_config)
 
 (load-library "g")
+
+(defun rhb_new_blog_post()
+  "Create a new blog post to rhburrows.blogspot.com and set up the appropriate modes"
+  (interactive)
+  (gblogger-new-entry "http://www.blogger.com/feeds/5958465983876364674/posts/default")
+  (turn-off-auto-fill))
