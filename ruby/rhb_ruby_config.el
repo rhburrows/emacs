b945f8d1783c1d23a721a913d4a1a41ee0075571
Ryan Burrows <rhburrows@gmail.com>
2010-04-06 23:44:35 -0700

    Add some whitespace rules to ruby

--- ruby/rhb_ruby_config.el
+++ ruby/rhb_ruby_config.el
@@ -25,9 +25,16 @@
 (ruby-block-mode t)
 (setq ruby-block-highlight-toggle t)
 
+;; Set up formatting
+;; Move this to other modes if necessary
+(setq whitespace-style '(trailing line space-before-tab
+				  indentation space-after-tab)
+      whitespace-line-column 80)
+
 (add-hook 'ruby-mode-hook
 	  '(lambda ()
 	     (inf-ruby-keys)
+	     (whitespace-mode 1)
 	     (define-key ruby-mode-map "\C-c#" 'comment-or-uncomment-region)
 	     (define-key ruby-mode-map (kbd "C-c i") 'ri)))
 
