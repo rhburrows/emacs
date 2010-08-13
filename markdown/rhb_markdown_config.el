95e12c9a0b6d18a644f36e3060b70551f9218a47
Ryan Burrows <rhburrows@crystalcommerce.com>
2010-08-13 11:27:58 -0700

    Add *.md to markdown mode

--- markdown/rhb_markdown_config.el
+++ markdown/rhb_markdown_config.el
@@ -4,3 +4,5 @@
   "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
       (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.md" . markdown-mode) auto-mode-alist))
