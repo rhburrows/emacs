618ad4c4d0a87aef2979c8d316ff4d29b4e5ab89
Ryan Burrows <rhburrows@crystalcommerce.com>
2010-11-03 08:41:25 -0700

    I'm pretty sure this wasn't working

--- elpa/rhb_package_config.el
+++ elpa/rhb_package_config.el
@@ -1,8 +1,5 @@
 (provide 'rhb_package_config)
 
-(setq package-archives ''(("ELPA" . "http://tromey.com/elpa/")
-                          ("gnu" . "http://elpa.gnu.org/packages/")))
-
 (when
     (load "package.el")
   (package-initialize))
