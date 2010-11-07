f538da8d1b273db9f90b76c5026fc859a5c6d62b
Ryan Burrows <rhburrows@gmail.com>
2010-11-06 21:16:57 -0700

    Steal auto-installing elpa stuff from starter-kit

--- elpa/rhb_package_config.el
+++ elpa/rhb_package_config.el
@@ -4,7 +4,38 @@
     (load "package.el")
   (package-initialize))
 
-(setq elpa-installed-packages
-      '(ruby-mode
+(setq rhb-installed-packages
+      '(inf-ruby
         rspec-mode
-        inf-ruby))
+        ruby-mode
+        smex
+        weblogger
+        xml-rpc))
+
+;; Stolen from the emacs-starter-kit
+(defun starter-kit-elpa-install ()
+  "Install all starter-kit packages that aren't installed."
+  (interactive)
+  (dolist (package rhb-installed-packages)
+    (unless (or (member package package-activated-list)
+                (functionp package))
+      (message "Installing %s" (symbol-name package))
+      (package-install package))))
+
+(defun esk-online? ()
+  "See if we're online.
+
+Windows does not have the network-interface-list function, so we
+just have to assume it's online."
+  (if (and (functionp 'network-interface-list)
+           (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+                         (member 'up (first (last (network-interface-info
+                                                   (car iface)))))))
+            (network-interface-list))
+    t))
+
+;; On your first run, this should pull in all the base packages.
+(when (esk-online?)
+  (unless package-archive-contents (package-refresh-contents))
+  (starter-kit-elpa-install))
