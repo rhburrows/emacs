3eea9cb047fbf32f4475dab92af1f59611f01ee0
Ryan Burrows <rhburrows@crystalcommerce.com>
2010-11-09 07:48:20 -0800

    Online checking doesn't seem to work

--- elpa/rhb_package_config.el
+++ elpa/rhb_package_config.el
@@ -22,20 +22,6 @@
       (message "Installing %s" (symbol-name package))
       (package-install package))))
 
-(defun esk-online? ()
-  "See if we're online.
-
-Windows does not have the network-interface-list function, so we
-just have to assume it's online."
-  (if (and (functionp 'network-interface-list)
-           (network-interface-list))
-      (some (lambda (iface) (unless (equal "lo" (car iface))
-                         (member 'up (first (last (network-interface-info
-                                                   (car iface)))))))
-            (network-interface-list))
-    t))
-
 ;; On your first run, this should pull in all the base packages.
-(when (esk-online?)
-  (unless package-archive-contents (package-refresh-contents))
-  (starter-kit-elpa-install))
+(unless package-archive-contents (package-refresh-contents))
+(starter-kit-elpa-install)
