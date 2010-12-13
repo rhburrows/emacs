0370ddecd34b57f39dd299a25720db74c23f8d37
Ryan Burrows <rhburrows@gmail.com>
2010-12-12 20:36:58 -0800

    Update org-mode

--- rhb_org_config.el
+++ rhb_org_config.el
@@ -1,8 +1,19 @@
 (provide 'rhb_org_config)
 
+(setq load-path (cons "~/.emacs.d/org/lisp" load-path))
+(setq load-path (cons "~/.emacs.d/org/contrib/lisp" load-path))
+
+(require 'org-install)
+
 (setq org-directory "~/Dropbox/org")
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
-(setq org-log-done t)
+
+(setq org-agenda-files '("~/Dropbox/org/"))
+
+;; Mobile org stuff
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/inbox.org")
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
