d17c6440ebbf50ffe8aff439591818f375052a35
Ryan Burrows <rhburrows@gmail.com>
2011-11-20 13:28:20 -0500

    I don't use this function

--- rhb_eshell.el
+++ rhb_eshell.el
@@ -1,12 +1,5 @@
 (provide 'rhb_eshell)
 
-(defun eshell/rails-model (&rest args)
-  "Open a rails model if this is the project directory"
-  (mapc #'find-file (mapcar
-                     (lambda (name)
-                       (expand-file-name (concat "./app/models/" name ".rb")))
-                     (eshell-flatten-list (reverse args)))))
-
 ;; Add extra stuff to path
 (setenv "PATH" (concat
                 "/usr/local/bin:"
